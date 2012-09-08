d9c4b9c5add384e5575543a365d8b06f291af846
Dustin Conrad <dustinaconrad@gmail.com>
2012-09-07 22:32:52 -0400

    refactor to follow github go project conventions

--- scripts/setenv.ps1
+++
@@ -1,8 +0,0 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath | Split-Path
-
-$goPath = $env:GOPATH
-
-if (-not $goPath.contains($dir)) {
-    $env:GOPATH = "$goPath;$dir"
-}
